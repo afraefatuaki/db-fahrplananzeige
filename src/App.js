1c25952743588047b92b72015052fcf304f0f1a9
Afrae El Fatuaki <afrae@Renes-iMac.fritz.box>
2022-12-08 16:30:08 +0100

    1.start react app 2. create timeTable mocked data 3. add TimeTable in a component folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TimeTable from './components.js/TimeTable';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TimeTable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components.js/TimeTable.js
@@ -0,0 +1,12 @@
+import TimeTableData from '../timeTableData.json'
+
+
+
+export default function TimeTable() {
+    console.log('data',TimeTableData )
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
